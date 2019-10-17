d5daaba49c90dde5cb13cdbe9d6a760804a77d59
kbaba1001 <kbaba1001@users.noreply.github.com>
2019-10-18 08:30:51 +0900

    run clojure outof docker

---
+++ src/try_hugsql/db/characters.clj
@@ -0,0 +1,29 @@
+(ns try-hugsql.db.characters
+  (:require [hugsql.core :as hugsql]))
+
+; TODO jdbc で postgres に繋げられるようにする
+
+;; The path is relative to the classpath (not proj dir!),
+;; so "src" is not included in the path.
+;; The same would apply if the sql was under "resources/..."
+;; Also, notice the under_scored path compliant with
+;; Clojure file paths for hyphenated namespaces
+(hugsql/def-db-fns "try_hugsql/db/sql/characters.sql")
+
+;; For most HugSQL usage, you will not need the sqlvec functions.
+;; However, sqlvec versions are useful during development and
+;; for advanced usage with database functions.
+(hugsql/def-sqlvec-fns "try_hugsql/db/sql/characters.sql")
+
+; (characters-by-ids-specify-cols-sqlvec {:ids [1 2], :cols ["name" "specialty"]})  
+
+; ;;=>
+; ["select name, specialty from characters
+;   where id in (?,?)"
+;  1
+;  2]
+
+; (characters/characters-by-ids-specify-cols db
+;                                            {:ids [1 2], :cols ["name" "specialty"]})  ;;=>
+; ({:name "Westley", :specialty "love"}
+;  {:name "Buttercup", :specialty "beauty"})
